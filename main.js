dd41f5b36a38b34c38f52f77896baba0f6bbeb0c
singhjason96 <jason.singh@baruchmail.cuny.edu>
2019-07-15 18:53:27 -0400

    fixed resume, fixed back button, and tested color schemes

--- main.js
+++ main.js
@@ -10,5 +10,10 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 2000); 
+}
+
+
+function goBack() {
+  window.history.back();
 }
